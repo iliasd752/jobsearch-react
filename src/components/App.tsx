14d9e5aa4fc28c9b47189a48443d683a3a536026
Ilias <ilias.dinia@outlook.com>
2025-02-27 13:55:41 +0100

    Deriving state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,7 +16,7 @@ import { useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
-  const { jobItems, isLoading } = useJobItems(searchText);
+  const { jobItemsSliced, isLoading } = useJobItems(searchText);
 
   return (
     <>
@@ -36,7 +36,7 @@ function App() {
             <ResultsCount />
             <SortingControls />
           </SidebarTop>
-          <JobList jobItems={jobItems} isLoading={isLoading} />
+          <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
           <PaginationControls />
         </Sidebar>
         <JobItemContent />
