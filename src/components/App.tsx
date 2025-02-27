8124c22f3aff44e8494e801f3c2c22e03af0565a
Ilias <ilias.dinia@outlook.com>
2025-02-27 13:19:10 +0100

    implementing loading spinner

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,15 +16,19 @@ import JobList from "./JobList";
 function App() {
   const [searchText, setSearchText] = useState("");
   const [jobItems, setJobItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!searchText) return;
 
     const fetchData = async () => {
+      setIsLoading(true);
+
       const response = await fetch(
         `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
       );
       const data = await response.json();
+      setIsLoading(false);
       setJobItems(data.jobItems);
     };
 
