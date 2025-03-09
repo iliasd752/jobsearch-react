b7a24375c6195a682760af7250a22b6e9d86d646
Ilias <ilias.dinia@outlook.com>
2025-03-09 14:29:42 +0100

    fixed the spinner loading when there's no fetch request + implemented error handling for the fetch request

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -10,12 +10,16 @@ type JobItemApiResponse = {
 
 const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
   const response = await fetch(`${BASE_API_URL}/${id}`);
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
   const data = await response.json();
   return data;
 };
 
 export function useJobItem(id: number | null) {
-  const { data, isLoading } = useQuery(
+  const { data, isInitialLoading } = useQuery(
     ["job-item", id],
     () => (id ? fetchJobItem(id) : null),
     {
@@ -23,10 +27,13 @@ export function useJobItem(id: number | null) {
       refetchOnWindowFocus: false,
       retry: false,
       enabled: Boolean(id),
-      onError: () => {},
+      onError: (error) => {
+        console.log(error);
+      },
     }
   );
   const jobItem = data?.jobItem;
+  const isLoading = isInitialLoading;
   return { jobItem, isLoading } as const;
 }
 
