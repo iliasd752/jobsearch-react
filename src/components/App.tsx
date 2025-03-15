1045e1c361e0348673712c34f82047429af7a665
Ilias <ilias.dinia@outlook.com>
2025-03-15 13:39:25 +0100

    implemented sorting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,21 +15,29 @@ import JobList from "./JobList";
 import { useDebounce, useJobItems } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
 import { RESULTS_PER_PAGE } from "../lib/constants";
+import { SortBy } from "../lib/types";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const debouncedSearchText = useDebounce(searchText, 250);
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
   const [currentPage, setCurrentPage] = useState(1);
-  const [sortBy, setSortBy] = useState("relevant");
+  const [sortBy, setSortBy] = useState<SortBy>("relevant");
 
   const totalNumberOfResults = jobItems?.length || 0;
   const totalNumberOfPages = totalNumberOfResults / 7;
-  const jobItemsSliced =
-    jobItems?.slice(
-      currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
-      currentPage * RESULTS_PER_PAGE
-    ) || [];
+  const jobItemsSorted =
+    jobItems?.sort((a, b) => {
+      if (sortBy === "relevant") {
+        return b.relevanceScore - a.relevanceScore;
+      } else {
+        return a.daysAgo - b.daysAgo;
+      }
+    }) || [];
+  const jobItemsSortedAndSliced = jobItemsSorted.slice(
+    currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+    currentPage * RESULTS_PER_PAGE
+  );
 
   const handleChangePage = (direction: "next" | "previous") => {
     if (direction === "next") {
@@ -39,7 +47,8 @@ function App() {
     }
   };
 
-  const handleChangeSortBy = (newSortBy) => {
+  const handleChangeSortBy = (newSortBy: SortBy) => {
+    setCurrentPage(1);
     setSortBy(newSortBy);
   };
 
@@ -59,9 +68,9 @@ function App() {
         <Sidebar>
           <SidebarTop>
             <ResultsCount totalNumberOfResults={totalNumberOfResults} />
-            <SortingControls onClick={handleChangeSortBy} />
+            <SortingControls sortBy={sortBy} onClick={handleChangeSortBy} />
           </SidebarTop>
-          <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
+          <JobList jobItems={jobItemsSortedAndSliced} isLoading={isLoading} />
           <PaginationControls
             currentPage={currentPage}
             totalNumberOfPages={totalNumberOfPages}
--- src/components/SortingControls.tsx
+++ src/components/SortingControls.tsx
@@ -1,18 +1,22 @@
-export default function Sorting({ onClick }) {
+export default function Sorting({ onClick, sortBy }) {
   return (
     <section className="sorting">
       <i className="fa-solid fa-arrow-down-short-wide"></i>
 
       <button
         onClick={() => onClick("relevant")}
-        className="sorting__button sorting__button--relevant"
+        className={`sorting__button sorting__button--relevant ${
+          sortBy === "relevant" ? "sorting__button--active" : ""
+        }`}
       >
         Relevant
       </button>
 
       <button
         onClick={() => onClick("recent")}
-        className="sorting__button sorting__button--recent"
+        className={`sorting__button sorting__button--recent ${
+          sortBy === "recent" ? "sorting__button--recent" : ""
+        }`}
       >
         Recent
       </button>
